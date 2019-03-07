6e52031ff9cf5f7a1df3eac9e96548c534b544a6
Keaton <ksmith36@student.waubonsee.edu>
2019-03-07 11:57:21 -0600

    Added product feature and padded output

--- quotient.py
+++ quotient.py
@@ -3,6 +3,7 @@ PROMPT_FIRST_NUMBER = "Please enter a number: "
 PROMPT_SECOND_NUMBER = "Please enter another number: "
 
 RESULT_MESSAGE_DIVISION = "The quotient is {0} with a remainder of {1}"
+RESULT_MESSAGE_MULTIPLICATION = "The product of the numbers is {0:.5g}"
 
 SET_OF_NUMBERS = 2 #Set of numbers to get from user and run operations on
 
@@ -10,7 +11,11 @@ def main():
     for i in range(SET_OF_NUMBERS):
         number1, number2 = get_two_numbers()
 
+        print()
         print_quotient_and_remainder(number1, number2)
+        print()
+        print_product(number1, number2)
+        print()
         print()
 
 
@@ -34,6 +39,10 @@ def print_quotient_and_remainder(dividend, divisor):
     quotientFloor = dividend // divisor
     quotientRemainder = dividend % divisor
     print(RESULT_MESSAGE_DIVISION.format(format(quotientFloor, '.0f'), '{0:.5g}'.format(quotientRemainder)))
+
+def print_product(multiplicand, multiplier):
+    product = multiplicand * multiplier
+    print(RESULT_MESSAGE_MULTIPLICATION.format(product))
     
 
 #Call main function
